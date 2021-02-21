ee71416bc5812798282e990fbdd479f050a5d63f
Ubuntu <ubuntu@ip-172-31-42-56.eu-west-3.compute.internal>
2021-02-21 12:06:11 +0000

    changed cargo env sourcing

--- .zshenv
+++ .zshenv
@@ -158,4 +158,3 @@ function cheat { curl cheat.sh/"$@"; }
 function compressgif {
     ffmpeg -i $1 -vf scale=320:-1 -r 10 -f image2pipe -vcodec ppm - | convert -delay 5 -loop 0 - $2
 }
-source "$HOME/.cargo/env"
--- main.sh
+++ main.sh
@@ -88,6 +88,10 @@ then
 	cd ~/.pyenv && src/configure && make -C src && cd -
 fi
 
+if [[! "$HOME/.cargo/env "]]
+then
+	source "$HOME/.cargo/env"
+fi
 # gitfetch https://github.com/pyenv/pyenv.git ~/.pyenv
 # When started as 'evim', evim.vim will already have done these settings, bail out.
 # if v:progname =~? "evim"
