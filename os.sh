90bb14f44e5f6f8ab604a823e9418f4dc4a2d09d
marc <marc.partensky@gmail.com>
2021-03-09 00:36:34 +0100

    check of os

---
+++ os.sh
@@ -0,0 +1,14 @@
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  local DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+  if [[ ${DISTRIB} = "Ubuntu"* ]]; then
+    if uname -a | grep -q '^Linux.*Microsoft'; then
+      # ubuntu via WSL Windows Subsystem for Linux
+    else
+      # native ubuntu
+    fi
+  elif [[ ${DISTRIB} = "Debian"* ]]; then
+    # debian
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS OSX
+fi
