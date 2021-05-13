e4d78457f17294e6a7000f81c51afe7baac2cd72
marc <marc.partensky@gmail.com>
2021-05-13 15:21:09 +0000

    no symlink alacritty

--- main.sh
+++ main.sh
@@ -137,7 +137,7 @@ ln -snf ${0:a:h}/.gitconfig ~
 ln -snf ${0:a:h}/.zshenv ~
 ln -snf ${0:a:h}/.p10k.zsh ~
 ln -snf ${0:a:h}/.tmux.conf ~
-ln -snf ${0:a:h}/.alacritty.yml ~/.config/alacritty/alacritty.yml
+# ln -snf ${0:a:h}/.alacritty.yml ~/.config/alacritty/alacritty.yml
 # ln -snf ${0:a:h}/.docker/config.json ~/.docker/config.json
 
 # source ${0:a:h}/setup.sh
