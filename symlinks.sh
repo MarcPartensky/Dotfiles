3be2460a34a0b012c717f99a967aa37824b1d1b4
marc <marc.partensky@gmail.com>
2021-05-15 17:02:37 +0200

    fixed symlink

--- symlinks.sh
+++ symlinks.sh
@@ -3,5 +3,5 @@ ln -snf ${0:a:h}/.zshenv ~
 ln -snf ${0:a:h}/.p10k.zsh ~
 ln -snf ${0:a:h}/.tmux.conf ~
 mkdir -p ~/.config/nvim
-ln -snf ${0:a:h}/.alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -snf ${0:a:h}/alacritty.yml ~/.config/alacritty/alacritty.yml
 # ln -snf ${0:a:h}/.docker/config.json ~/.docker/config.json
