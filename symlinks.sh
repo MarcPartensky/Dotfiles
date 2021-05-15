80ee0aadf91845496b2f7869a0b89f7495b9f017
marc <marc.partensky@gmail.com>
2021-05-15 20:39:24 +0000

    failed to create right directory cuz im dumb

--- symlinks.sh
+++ symlinks.sh
@@ -2,6 +2,6 @@ ln -snf ${0:a:h}/.gitconfig ~
 ln -snf ${0:a:h}/.zshenv ~
 ln -snf ${0:a:h}/.p10k.zsh ~
 ln -snf ${0:a:h}/.tmux.conf ~
-mkdir -p ~/.config/nvim
+mkdir -p ~/.config/alacritty
 ln -snf ${0:a:h}/alacritty.yml ~/.config/alacritty/alacritty.yml
 # ln -snf ${0:a:h}/.docker/config.json ~/.docker/config.json
