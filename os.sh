63b6aee4098466afe1cd0a443b42aa0d349fd44b
marc <marc.partensky@gmail.com>
2021-05-28 10:18:48 +0000

    fix os.sh

--- os.sh
+++ os.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
 	export SYSTEM="osx"
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
 	export SYSTEM="windows"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
 	export SYSTEM="linux"
 fi
