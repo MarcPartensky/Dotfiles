0a3bf04dc70c3cedac13ec8d0931b8135f1a9026
marc <marc.partensky@gmail.com>
2021-07-10 18:39:52 +0200

    quick.sh

---
+++ quick.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+eval "$(curl -s https://raw.githubusercontent.com/MarcPartensky/Dotfiles/main/aliases.sh)"
+eval "$(curl -s https://raw.githubusercontent.com/MarcPartensky/Dotfiles/main/functions.sh)"
+eval "$(curl -s https://raw.githubusercontent.com/MarcPartensky/Dotfiles/main/exports.sh)"
