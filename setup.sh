3083dc4df73ceb7378f6bc20b28716b1c7188464
marc <marc.partensky@gmail.com>
2021-11-04 10:19:28 -0400

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 eval `curl -sSL marcpartensky.com//brew`
+mkdir -p $HOME/git
+mkdir -p $HOME/.config
+
+git clone https://github.com/marcpartensky/dotfiles $HOME/git
+git clone https://github.com/marcpartensky/nvim $HOME/.config
+
