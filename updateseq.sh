e23abdceeb1beead2d0cbdacdb77a56f353da185
marc <marc.partensky@gmail.com>
2021-11-18 19:35:27 +0100

    update.sh + update-seq.sh

--- updateseq.sh
+++ updateseq.sh
@@ -14,43 +14,43 @@ main() {
 	if command -v brew; then
 		$(brew upgrade -f &&
 			brew autoremove &&
-			brew cleanup) | sudo tee $LOG_FOLDER/update_brew.log >> $LOG_FILE 2>&1
+			brew cleanup) | sudo tee $LOG_FOLDER/update_brew.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v apt; then
 		$(apt --fix-broken install &&
 			apt update &&
 			apt upgrade -y &&
-			apt autoremove) | sudo tee $LOG_FOLDER/update_apt.log >> $LOG_FILE 2>&1
+			apt autoremove) | sudo tee $LOG_FOLDER/update_apt.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v dnf; then
 		$(sudo dnf upgrade -y &&
-			sudo dnf autoremove) sudo tee | $LOG_FOLDER/update_dnf.log >> $LOG_FILE 2>&1
+			sudo dnf autoremove) sudo tee | $LOG_FOLDER/update_dnf.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v yum; then
-		$(yum update) | sudo tee $LOG_FOLDER/update_yum.log >> $LOG_FILE 2>&1
+		$(yum update) | sudo tee $LOG_FOLDER/update_yum.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v zipper; then
 		$(zypper refresh &&
-			zypper update) | sudo tee $LOG_FOLDER/update_zypper.log >> $LOG_FILE 2>&1
+			zypper update) | sudo tee $LOG_FOLDER/update_zypper.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v emerge; then
 		$(emerge --sync &&
-			emerge --update --deep --with-bdeps=y @world) | sudo tee $LOG_FOLDER/update_emerge.log >> $LOG_FILE 2>&1
+			emerge --update --deep --with-bdeps=y @world) | sudo tee $LOG_FOLDER/update_emerge.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v pacman; then
-		pacman -Syu | sudo tee $LOG_FOLDER/update_pacman.log >> $LOG_FILE 2>&1
+		pacman -Syu | sudo tee $LOG_FOLDER/update_pacman.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v yay; then
-		yay -Syu | sudo tee $LOG_FOLDER/update_yay.log >> $LOG_FILE 2>&1
+		yay -Syu | sudo tee $LOG_FOLDER/update_yay.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v apk; then
-		apk -U upgrade | sudo tee $LOG_FOLDER/update_apk.log >> $LOG_FILE 2>&1
+		apk -U upgrade | sudo tee $LOG_FOLDER/update_apk.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v snap; then
-		snap update | sudo tee $LOG_FOLDER/update_snap.log >> $LOG_FILE 2>&1
+		snap update | sudo tee $LOG_FOLDER/update_snap.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v pipupgrade; then
-		pipupgrade | sudo tee $LOG_FOLDER/update_pipupgrade.log >> $LOG_FILE 2>&1
+		pipupgrade | sudo tee $LOG_FOLDER/update_pipupgrade.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v npm; then
 		$(npm install npm@latest -g &&
@@ -58,38 +58,38 @@ main() {
 			npm cache clean -f &&
 			npm update -g &&
 			npm audit fix &&
-			npm prune) | sudo tee $LOG_FOLDER/update_npm.log >> $LOG_FILE 2>&1
+			npm prune) | sudo tee $LOG_FOLDER/update_npm.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v yarn; then
 		$(yarn add --dev yarn-upgrade-all &&
 			yarn yarn-upgrade-all &&
-			yarn upgrade --latest) | sudo tee $LOG_FOLDER/update_yarn.log >> $LOG_FILE 2>&1
+			yarn upgrade --latest) | sudo tee $LOG_FOLDER/update_yarn.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v pip; then
 		$($(pip install -U pip &&
 			pip freeze > /tmp/pipfreeze.txt) &&
 		for lib in $(cat /tmp/pipfreeze.txt); do
 			pip install -U $lib &
-		done) | sudo tee $LOG_FOLDER/update_pip.log >> $LOG_FILE 2>&1
+		done) | sudo tee $LOG_FOLDER/update_pip.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v pip3; then
-		pip3 install -U pip | sudo tee $LOG_FOLDER/update_pip3.log >> $LOG_FILE 2>&1
+		pip3 install -U pip | sudo tee $LOG_FOLDER/update_pip3.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v pip2; then
-		pip2 install -U pip | sudo tee $LOG_FOLDER/update_pip2.log >> $LOG_FILE 2>&1
+		pip2 install -U pip | sudo tee $LOG_FOLDER/update_pip2.log >> $LOG_FILE 2>&1 &
 	fi
 	if command -v cargo; then
 		$(cargo install cargo-update &&
-			cargo install-update -a) | sudo tee $LOG_FOLDER/update_cargo.log >> $LOG_FILE 2>&1
+			cargo install-update -a) | sudo tee $LOG_FOLDER/update_cargo.log >> $LOG_FILE 2>&1 &
 		# cargo install $(cargo install --list | egrep '^[a-z0-9_-]+ v[0-9.]+:$' | cut -f1 -d' ')
 	fi
 	if command -v go; then
 		if [ -z "$GOPATH" ]; then
-			go get -u all | sudo tee $LOG_FOLDER/update_go.log >> $LOG_FILE 2>&1
+			go get -u all | sudo tee $LOG_FOLDER/update_go.log >> $LOG_FILE 2>&1 &
 		fi
 	fi
 	if command -v tldr; then
-		tldr --update | sudo tee $LOG_FOLDER/update_tldr.log >> $LOG_FILE 2>&1
+		tldr --update | sudo tee $LOG_FOLDER/update_tldr.log >> $LOG_FILE 2>&1 &
 	fi
 }
 
