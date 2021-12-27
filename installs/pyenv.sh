1ae8a62fc6267ee36915efeb8b4a7ca90bbd2293
Marc Partensky <marc.partensky@gmail.com>
2021-12-27 02:11:11 +0100

    pyenv

--- installs/pyenv.sh
+++ installs/pyenv.sh
@@ -10,5 +10,9 @@ if [[ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]]
 then
 	git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 fi
+if [[ ! -d $(pyenv root)/plugins/pyenv-update ]]
+then
+    git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
+fi
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
