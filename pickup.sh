d02684461253b1ba490451ffccd698651425aa54
Marc Partensky <marc.partensky@gmail.com>
2021-12-29 12:32:28 +0100

    Documented

--- pickup.sh
+++ pickup.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-export C21_DUMP_PATH="$HOME/Downloads/pickup/dump"
+# Choose where to store the dump files
+export C21_DUMP_PATH="$HOME/.pickup/dump"
 [[ -d $C21_DUMP_PATH ]] || mkdir -p $C21_DUMP_PATH
 
+# mongodump and mongorestore with docker for portability
 alias dmongodump="docker run --network host -v $C21_DUMP_PATH:/srv --rm --entrypoint mongodump mongo"
 alias dmongorestore="docker run --network host -v $C21_DUMP_PATH:/srv --rm --entrypoint mongorestore mongo"
+
+# Parameters to connect to remote databases
 c21_mongo_params=(--host srvlh-mdb-b1.paris.pickup.local:45000 --db colis21_events --gzip -u hprod_RO -p Iv8E2k4Ptu7icBlRaq5A --authenticationDatabase admin)
 octopus_mongo_params=(--host srvlh-mdb-b2.paris.pickup.local:45014 --db kraken --gzip -u hprod_RO -p Iv8E2k4Ptu7icBlRaq5A --authenticationDatabase admin)
 tia21_mongo_params=(--host srvlh-mdb-b1.paris.pickup.local:45004 --db kraken --gzip -u hprod_RO -p Iv8E2k4Ptu7icBlRaq5A --authenticationDatabase admin)
 
+# Dump and restore commands
 dumpc21() {
 	dmongodump $c21_mongo_params[@] --archive=/srv/colis21_events_dump_$TIMESTAMP.bzip
 }
@@ -37,8 +42,40 @@ restore() {
     dmongorestore --gzip --archive=/srv/$1 --drop
 }
 
+# Update commands that combine dump and restore
+updatec21() {
+	TIMESTAMP=$(date +%s)
+	echo Backing up local mongo
+	echo Dumping local c21 for backup
+	dumpc21local
+	echo Dumping remote c21 test-v
+	dumpc21
+	echo Restoring local c21
+	restorec21
+}
+updatetia21() {
+	TIMESTAMP=$(date +%s)
+	echo Dumping local kraken test-v for backup
+	dumptia21local
+	echo Dumping remote kraken test-v
+	dumptia21
+	echo Restoring local tia21
+	restoretia21
+}
+updateoctopus() {
+	TIMESTAMP=$(date +%s)
+	echo Backing up local mongo
+	echo Dumping local kraken test-v for backup
+	dumptia21local
+	echo Dumping remote kraken test-v
+	dumpoctopus
+	echo Restoring local tia21
+	restoreoctopus
+}
+
+# Update both c21 and tia21 (or octopus)
 updatec21tia21() {
-	TIMESTAMP=$(timestamp)
+	TIMESTAMP=$(date +%s)
 	echo Backing up local mongo
 	echo Dumping local colis21 test-v for backup
 	dumpc21local
@@ -56,7 +93,7 @@ updatec21tia21() {
 }
 
 updatec21octopus() {
-	TIMESTAMP=$(timestamp)
+	TIMESTAMP=$(date +%s)
 	echo Backing up local mongo
 	echo Dumping local colis21 test-v for backup
 	dumpc21local
@@ -71,37 +108,5 @@ updatec21octopus() {
 	restoreoctopus
 	echo Restoring local colis21
 	restorec21
-}
-
-
-updatec21() {
-	echo Backing up local mongo
-	TIMESTAMP=$(timestamp)
-	echo Dumping local c21 for backup
-	dumpc21local
-	echo Dumping remote c21 test-v
-	dumpc21
-	echo Restoring local c21
-	restorec21
-}
-updatetia21() {
-	echo Backing up local mongo
-	TIMESTAMP=$(timestamp)
-	echo Dumping local kraken test-v for backup
-	dumptia21local
-	echo Dumping remote kraken test-v
-	dumptia21
-	echo Restoring local tia21
-	restoretia21
-}
-updateoctopus() {
-	echo Backing up local mongo
-	TIMESTAMP=$(timestamp)
-	echo Dumping local kraken test-v for backup
-	dumptia21local
-	echo Dumping remote kraken test-v
-	dumpoctopus
-	echo Restoring local tia21
-	restoreoctopus
 }
 
