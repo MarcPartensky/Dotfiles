0e3e31bf0c5c784d444e1409ab4a85cd141d92f0
Marc Partensky <marc.partensky@gmail.com>
2022-02-28 01:20:03 +0100

    pathadd

--- functions.sh
+++ functions.sh
@@ -826,3 +826,9 @@ cleansubvolumes() {
         btrfs subvolume delete $subvolume
     done
 }
+
+pathadd() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        PATH="${PATH:+"$PATH:"}$1"
+    fi
+}
