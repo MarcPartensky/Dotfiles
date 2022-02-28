b82c28af3f82980f897911551d85261214a31b80
Marc Partensky <marc.partensky@gmail.com>
2022-02-28 10:29:56 +0100

    fix symlink

--- symlinks.sh
+++ symlinks.sh
@@ -8,8 +8,8 @@ ln -snf ${0:a:h}/.yabairc $HOME
 ln -snf ${0:a:h}/.skhdrc $HOME
 mkdir -p $HOME/.config/bspwm
 ln -snf ${0:a:h}/bspwmrc $HOME/.config/bspwm/bspwmrc
-mkdir -p $HOME/.config/sxhkdrc
-ln -snf ${0:a:h}/sxhkdrc $HOME/.config/sxhkd
+mkdir -p $HOME/.config/sxhkd
+ln -snf ${0:a:h}/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 mkdir -p $HOME/.config/alacritty
 ln -snf ${0:a:h}/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 # ln -snf ${0:a:h}/.docker/config.json $HOME/.docker/config.json
