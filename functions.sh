4e092b08a2cf3a942d19c915f132840a8545005a
Marc Partensky <marc.partensky@gmail.com>
2022-03-28 20:43:14 +0200

    capture

--- functions.sh
+++ functions.sh
@@ -858,3 +858,9 @@ deploy() {
 runas() {
     su - $1 -c ${@:2}
 }
+
+capturehttp() {
+    tshark  'tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'
+ -R'http.request.method == "GET" || http.request.method == "HEAD"'
+    tail -f /var/tmp/wireshark*
+}
