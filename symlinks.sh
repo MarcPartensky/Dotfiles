afb149ba9d7236bcf6f6410bdea8128f31c3ad50
Marc Partensky <marc.partensky@gmail.com>
2022-05-03 02:24:53 +0200

    dots

--- symlinks.sh
+++ symlinks.sh
@@ -3,8 +3,8 @@
 # ln -snf ${0:a:h}/.zshenv $HOME
 # ln -snf ${0:a:h}/.p10k.zsh $HOME
 # ln -snf ${0:a:h}/.tmux.conf $HOME
-ln -snf ${0:a:h}/.tmate.conf $HOME
-ln -snf ${0:a:h}/.yabairc $HOME
+# ln -snf ${0:a:h}/.tmate.conf $HOME
+# ln -snf ${0:a:h}/.yabairc $HOME
 # ln -snf ${0:a:h}/.skhdrc $HOME
 # mkdir -p $HOME/.config/bspwm
 # ln -snf ${0:a:h}/bspwmrc $HOME/.config/bspwm/bspwmrc
@@ -13,5 +13,5 @@ ln -snf ${0:a:h}/.yabairc $HOME
 # mkdir -p $HOME/.config/alacritty
 # ln -snf ${0:a:h}/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 # ln -snf ${0:a:h}/.docker/config.json $HOME/.docker/config.json
-mkdir -p $HOME/.config/polybar
-ln -snf ${0:a:h}/polybar_config.ini $HOME/.config/polybar/config.ini
+# mkdir -p $HOME/.config/polybar
+# ln -snf ${0:a:h}/polybar_config.ini $HOME/.config/polybar/config.ini
