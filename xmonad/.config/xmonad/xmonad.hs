379e21cb2756a54bbaa78faad393b56c66c084c6
Marc Partensky <marc.partensky@gmail.com>
2022-09-05 22:28:36 +0200

    super + enter = terminal

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,18 +1,22 @@
 import XMonad
 
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeysP, removeKeysP)
 import XMonad.Util.Ungrab
 
 _terminal = "alacritty"
-
 main :: IO()
 main = xmonad $ def
     {
+        terminal = _terminal,
         modMask = mod4Mask -- Rebind Mod to Super
     }
     `additionalKeysP`
     [
         ("M-f", spawn "firefox-nightly"),
-        ("M-a", spawn _terminal),
+        ("M-<Return>", spawn _terminal),
         ("M-d", spawn "rofi -show run")
+    ]
+    `removeKeysP`
+    [
+        ("M-<Shift>-<Return>")
     ]
