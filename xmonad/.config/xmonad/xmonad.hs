9ea2b16ba6cfbb6f77c2e035d975e916fdc1dfdb
Marc Partensky <marc.partensky@gmail.com>
2022-09-05 22:05:57 +0200

    firefox, alacritty and rofi for starter

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,10 +1,16 @@
 import XMonad
 
--- import XMonad.Util.EZConfig (additionalKeysP)
--- import XMonad.Util.Ungrab
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Ungrab
 
 main :: IO()
 main = xmonad $ def
     {
         modMask = mod4Mask -- Rebind Mod to Super
     }
+    `additionalKeysP`
+    [
+        ("M-f", spawn "firefox-nightly"),
+        ("M-a", spawn "alacritty"),
+        ("M-d", spawn "rofi -show run")
+    ]
