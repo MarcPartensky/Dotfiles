c55cc8b5c387d8850d46623f95ed0cf45a82f741
Marc Partensky <marc.partensky@gmail.com>
2022-09-05 22:39:49 +0200

    statusbar

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -2,6 +2,10 @@ import XMonad
 
 import XMonad.Util.EZConfig (additionalKeysP, removeKeysP)
 import XMonad.Util.Ungrab
+import Xmonad.Hooks.DynamicLog
+import Xmonad.Hooks.StatusBar
+import Xmonad.Hooks.StatusBar.PP
+import XMonad.Layout.ThreeColumns
 
 _terminal = "alacritty"
 main :: IO()
@@ -15,8 +19,16 @@ main = xmonad $ def
         ("M-f", spawn "firefox-nightly"),
         ("M-<Return>", spawn _terminal),
         ("M-d", spawn "rofi -show run")
+
     ]
     `removeKeysP`
     [
         ("M-<Shift>-<Return>")
     ]
+
+--_layout = tiled ||| Mirror tiled ||| Full
+--    where
+--        tiled = Tall nmaster delta ratio
+--        nmaster = 1
+--        ratio = 1/2
+--        delta = 3/100
