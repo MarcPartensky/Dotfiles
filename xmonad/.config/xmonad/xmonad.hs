0fc7380959143b1fe3632f3c1590d90a4c3458f4
Marc Partensky <marc.partensky@gmail.com>
2022-09-05 21:56:15 +0200

    xmonad mod = super

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,4 +1,10 @@
 import XMonad
 
-import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
+-- import XMonad.Util.EZConfig (additionalKeysP)
+-- import XMonad.Util.Ungrab
+
+main :: IO()
+main = xmonad $ def
+    {
+        modMask = mod4Mask -- Rebind Mod to Super
+    }
