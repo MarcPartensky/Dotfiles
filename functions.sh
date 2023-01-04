2abd0b2c3630c8e32ef3098645f3160f9a7d0f60
Marc Partensky <marc.partensky@gmail.com>
2023-01-04 21:31:37 +0100

    docker compose

--- functions.sh
+++ functions.sh
@@ -20,22 +20,22 @@ cd() {
 dc() {
     # export COMPOSE_FILE="$PROGRAMS_DOCKER_PATH/stacks/docker-compose.yml"
 	if [ "$1" = "-f" ]; then
-        docker-compose --file ${@: 2}
+        docker compose --file ${@: 2}
     else
 		if [ -f $PWD/docker-compose.yml ]; then
-			docker-compose --file $PWD/docker-compose.yml $@
+			docker compose --file $PWD/docker-compose.yml $@
 		else
-			docker-compose $@
+			docker compose $@
 			if [ "$1" = "up" ]; then
 				if [ "$2" = "-d" ]; then
-					docker-compose logs -f ${@: 3}
+					docker compose logs -f ${@: 3}
 				else
-					docker-compose logs -f ${@: 2}
+					docker compose logs -f ${@: 2}
 				fi
 			elif [ "$1" = "start" ]; then
-				docker-compose logs -f ${@: 2}
+				docker compose logs -f ${@: 2}
 			elif [ "$1" = "restart" ]; then
-				docker-compose logs -f ${@: 2}
+				docker compose logs -f ${@: 2}
 			fi
 		fi
 	fi
