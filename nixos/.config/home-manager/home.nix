773ee88cb5aa0f811474992a38a6f8f27d8bf692
Marc Partensky <marc.partensky@gmail.com>
2023-07-04 14:26:21 +0200

    pkgs

--- nixos/.config/home-manager/home.nix
+++ nixos/.config/home-manager/home.nix
@@ -18,9 +18,9 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    "nmap"
-    "nvim"
-    "zsh"
+    pkgs.nmap
+    pkgs.nvim
+    pkgs.zsh
     # pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
