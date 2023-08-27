bef644f5eee26d2f6ff28833c308e08d0ae0daa7
Marc Partensky <marc.partensky@gmail.com>
2023-08-28 00:40:29 +0200

    packageOverrides only

--- nixos/.config/nixpkgs/config.nix
+++ nixos/.config/nixpkgs/config.nix
@@ -1,7 +1,14 @@
 {
-  nixpkgs.config.packageOverrides = pkgs: {
+  packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
   };
 }
+# {
+#   nixpkgs.config.packageOverrides = pkgs: {
+#     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+#       inherit pkgs;
+#     };
+#   };
+# }
