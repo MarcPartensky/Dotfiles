b00837b7d12f377c83c8884872bba94466440553
Marc Partensky <marc.partensky@gmail.com>
2023-09-11 07:31:34 +0200

    spotify maybe

--- nixos/.config/home-manager/home.nix
+++ nixos/.config/home-manager/home.nix
@@ -464,6 +464,9 @@
     ydotool
     # pkgs.hello
 
+    ### Proprietary
+    spotify
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
