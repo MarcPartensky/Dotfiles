a65fa6fe91b79f97a15932270c179933a95f7c13
Marc Partensky <marc.partensky@gmail.com>
2025-02-09 15:09:54 +0100

    nixos

--- nixos/.config/home-manager/flake.nix
+++ nixos/.config/home-manager/flake.nix
@@ -1,41 +1,71 @@
+# {
+#   description = "Home Manager configuration of marc";
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+#     home-manager = {
+#       url = "github:nix-community/home-manager";
+#       inputs.nixpkgs.follows = "nixpkgs";
+
+#       # # no news
+#       # lib.homeManagerConfiguration.disableHomeManagerNews = {
+#       #   # disabledModules = [ "misc/news.nix" ];
+#       #   config = {
+#       #     news.display = "silent";
+#       #     news.json = lib.mkForce { };
+#       #     news.entries = lib.mkForce [ ];
+#       #   };
+#       # };
+#     };
+
+#     hyprland.url = "github:hyprwm/Hyprland";
+#   };
+
+#   outputs = { nixpkgs, home-manager, hyprland, ... }:
+#     let
+#       system = "x86_64-linux";
+#       pkgs = nixpkgs.legacyPackages.${system};
+#     in {
+#       homeConfigurations."marc@tower" = home-manager.lib.homeManagerConfiguration {
+#         inherit pkgs;
+
+#         # Specify your home configuration modules here, for example,
+#         # the path to your home.nix.
+#         modules = [
+# 		./home.nix
+# 		hyprland.homeManagerModules.default
+# 		{wayland.windowManager.hyprland.enable = true;}
+# 	];
+#     };
+#   };
+# }
+
 {
-  description = "Home Manager configuration of marc";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-
-      # # no news
-      # lib.homeManagerConfiguration.disableHomeManagerNews = {
-      #   # disabledModules = [ "misc/news.nix" ];
-      #   config = {
-      #     news.display = "silent";
-      #     news.json = lib.mkForce { };
-      #     news.entries = lib.mkForce [ ];
-      #   };
-      # };
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { nixpkgs, home-manager, hyprland, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."marc@tower" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = {nixpkgs, home-manager, hyprland, ...}: {
+    homeConfigurations."marc@nixos" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-		./home.nix
-		hyprland.homeManagerModules.default
-		{wayland.windowManager.hyprland.enable = true;}
-	];
+      modules = [
+        {
+          # wayland.windowManager.hyprland = {
+          #   enable = true;
+          #   # set the flake package
+          #   # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+          # };
+        }
+        # ...
+      ];
     };
   };
 }
