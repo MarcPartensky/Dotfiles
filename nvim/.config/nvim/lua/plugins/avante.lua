c3c9223c884b93f4fcd47d7d462ed4edf04fb83f
marc partensky <marc@marcpartensky.com>
2025-07-24 20:16:19 +0200

    @

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -5,14 +5,18 @@ return {
     opts = {
         -- add any opts here
         -- for example
-        provider = "openai",
-        openai = {
+        -- provider = "openai",
+        providers = {
+          openai = {
             endpoint = "https://api.openai.com/v1",
             model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
             timeout = 30000,  -- timeout in milliseconds
-            temperature = 0,  -- adjust if needed
             max_tokens = 4096,
             -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+            extra_request_body = {
+              temperature = 0,  -- adjust if needed
+            },
+          },
         },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
